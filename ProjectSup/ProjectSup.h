1abd25b9c587843224685e911214fc6001cfa6be
Konrad Uciechowski <konraduciechowski@MacBook-Pro-Konrad-2.local>
2019-07-25 10:50:08 +0200

    initial commit

---
+++ ProjectSup/ProjectSup.h
@@ -0,0 +1,8 @@
+//! Project version number for ProjectSup.
+FOUNDATION_EXPORT double ProjectSupVersionNumber;
+
+//! Project version string for ProjectSup.
+FOUNDATION_EXPORT const unsigned char ProjectSupVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProjectSup/PublicHeader.h>
+
---
+++ ProjectSupExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectSupExamples
+//
+//  Created by Konrad Uciechowski on 25/07/2019.
+//  Copyright © 2019 ProjectSup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
